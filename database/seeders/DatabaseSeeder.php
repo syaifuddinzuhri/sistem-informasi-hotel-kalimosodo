a88d5046fceee5e5cbb53665f28583202fa5f9e6
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-05-25 22:38:33 +0700

    Fasilitas Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $fasilitas = [
+            ['name'  => 'AC', 'is_active' => 1],
+            ['name'  => 'Televisi', 'is_active' => 1],
+            ['name'  => 'Shower', 'is_active' => 1],
+            ['name'  => 'Kamar Mandi Pribadi', 'is_active' => 1],
+            ['name'  => 'Free Wifi', 'is_active' => 1],
+            ['name'  => 'Hair Dryer', 'is_active' => 1],
+            ['name'  => 'Brankas dalam kamar', 'is_active' => 1],
+            ['name'  => 'Bathub', 'is_active' => 1],
+        ];
+        DB::table('facility')->insert($fasilitas);
     }
 }
